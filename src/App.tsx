23edf5fcbff0715073db981c9a2b8d7d7e08a478
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-03-09 17:18:31 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="py-10 bg-slate-100 h-screen">
+      <h1 className="font-semibold font-sans text-center text-3xl">To Do</h1>
+
+      
+    </main>
+  )
+}
